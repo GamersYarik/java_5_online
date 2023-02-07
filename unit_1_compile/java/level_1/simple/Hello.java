bcba2d1e447d932aa304eb10a7229667250fb018
GamersYarik <GamersYarik@gmail.com>
2023-02-07 18:06:11 +0200

    first

---
+++ unit_1_compile/java/level_1/simple/Hello.java
@@ -0,0 +1,21 @@
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        byte bMax = Byte.MAX_VALUE;
+        byte bMin = Byte.MIN_VALUE;
+        short sMax = Short.MAX_VALUE;
+        short sMin = Short.MIN_VALUE;
+        int iMax = Integer.MAX_VALUE;
+        int iMin = Integer.MIN_VALUE;
+        long lMax = Long.MAX_VALUE;
+        long lMin = Long.MIN_VALUE;
+        System.out.println("bMax = " + bMax);
+        System.out.println("bMin = " + bMin);
+        System.out.println("sMax = " + sMax);
+        System.out.println("sMin = " + sMin);
+        System.out.println("iMax = " + iMax);
+        System.out.println("iMin = " + iMin);
+        System.out.println("lMax = " + lMax);
+        System.out.println("lMin = " + lMin);
+    }
+}
